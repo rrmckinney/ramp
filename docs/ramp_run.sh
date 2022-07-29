50582114ce1ee4be76207f246353c4cbb9b79eca
Reagan McKinney <rmckinney@a23-150.eos.ubc.ca>
2022-07-28 18:56:58 -0700

    add ramp data

--- docs/ramp_run.sh
+++ docs/ramp_run.sh
@@ -2,12 +2,9 @@
 source /Users/rmckinney/miniconda3/bin/activate
 conda activate /Users/rmckinney/opt/miniconda3/envs/wfrt
 
+make clean 
 
-make clean SPHINXBUILD='/Users/rmckinney/opt/miniconda3/envs/wfrt/python' '/Users/rmckinney/opt/miniconda3/envs/wfrt/sphinx-build'
-
-
-make html SPHINXBUILD='/Users/rmckinney/opt/miniconda3/envs/wfrt/python' '/Users/rmckinney/opt/miniconda3/envs/wfrt/sphinx-build'
-
+make html 
 
 git add .
 
