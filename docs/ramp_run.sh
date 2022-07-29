88ea6615be5e24beb93b39220ff3bd985fea10fe
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-07-29 15:56:03 -0700

    update ramp data

--- docs/ramp_run.sh
+++ docs/ramp_run.sh
@@ -3,7 +3,7 @@ source /Users/rmckinney/miniconda3/bin/activate
 conda activate /Users/rmckinney/opt/miniconda3/envs/wfrt
 
 cd /Users/rmckinney/ramp/docs
-
+ipython -c "%run data.ipynb"
 make -f Makefile clean
 make -f Makefile html
 
