318dbcf43d7d960b83a3276d2346ac739ede346e
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-08-05 09:26:09 -0700

    update ramp data

--- docs/source/func.py
+++ docs/source/func.py
@@ -22,9 +22,9 @@ def run():
         url =  link+hist.strftime("%Y-%m-%d")+"-"+station+".txt"
         if os.path.exists(str(hist)+"-1047.txt"): 
             dat = str(hist)+"-1047.txt"
-        elif i == 6:
-            os.remove(str(hist)+"-1047.txt")
-            dat = wget.download(url)
+        #elif i == 6:
+            #os.remove(str(hist)+"-1047.txt")
+            #dat = wget.download(url)
         else:
             dat = wget.download(url)
         
