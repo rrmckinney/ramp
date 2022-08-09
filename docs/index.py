0e6b80ce49d7d6b9bf8caa9e69b53c858b255efe
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-08-09 13:52:28 -0700

    update ramp data

--- docs/index.py
+++ docs/index.py
@@ -4,4 +4,4 @@ working_tree_dir = "/Users/rmckinney/ramp"
 repo = Repo(working_tree_dir)
 repo.git.add(A=True)
 repo.git.commit('-m', 'update ramp data')
-repo.git.push
+repo.git.push('origin','main')
