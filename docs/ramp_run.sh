f396c09870d6c962c93bfef8e9b20365b63bd8c0
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-08-24 12:44:42 -0700

    update ramp data

--- docs/ramp_run.sh
+++ docs/ramp_run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+export PATH="/Users/rmckinney/opt/miniconda3/bin:/Users/rmckinney/opt/miniconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin"
+export SHELL=/bin/sh
 source /Users/rmckinney/miniconda3/bin/activate
 conda activate /Users/rmckinney/opt/miniconda3/envs/wfrt
 
-cd /Users/rmckinney/ramp/docs
+#cd /Users/rmckinney/ramp/docs
 jupyter nbconvert  --execute --clear-output /Users/rmckinney/ramp/docs/source/data.ipynb
 rm  -r /Users/rmckinney/ramp/docs/source/data/*
 make -f Makefile clean
