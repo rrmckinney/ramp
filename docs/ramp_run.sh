50d691e6fb5d98d82ebaf28bda3da585218cef4a
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-08-24 13:36:41 -0700

    update ramp data

--- docs/ramp_run.sh
+++ docs/ramp_run.sh
@@ -2,7 +2,7 @@
 export PATH="/Users/rmckinney/opt/miniconda3/bin:/Users/rmckinney/opt/miniconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin:/usr/bin/git:/usr/bin/ssh"
 source /Users/rmckinney/miniconda3/bin/activate
 conda activate /Users/rmckinney/opt/miniconda3/envs/wfrt
-export GIT_SSH_COMMAND="ssh -i /Users/rmckinney/.ssh/id_ed25519.pub"
+export GIT_SSH_COMMAND="ssh -i /Users/rmckinney/.ssh/id_ed25519"
 
 #cd /Users/rmckinney/ramp/docs
 jupyter nbconvert  --execute --clear-output /Users/rmckinney/ramp/docs/source/data.ipynb
