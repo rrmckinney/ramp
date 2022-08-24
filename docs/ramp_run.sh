ff82b8fc28eca5497eba43e53203c99e1a36bc1f
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-08-24 13:16:29 -0700

    update ramp data

--- docs/ramp_run.sh
+++ docs/ramp_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH="/Users/rmckinney/opt/miniconda3/bin:/Users/rmckinney/opt/miniconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin"
+export PATH="/Users/rmckinney/opt/miniconda3/bin:/Users/rmckinney/opt/miniconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin:/usr/bin/git"
 export SHELL=/bin/sh
 source /Users/rmckinney/miniconda3/bin/activate
 conda activate /Users/rmckinney/opt/miniconda3/envs/wfrt
