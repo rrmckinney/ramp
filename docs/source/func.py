804aad3321ac88db3534a3a7ae41fa1f06ce32d9
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-09-15 16:00:16 -0700

    update ramp data

--- docs/source/func.py
+++ docs/source/func.py
@@ -45,7 +45,7 @@ def run():
             
             hist = hist + timedelta(days=1)
         except:
-            print("an error occured for"+hist.strftime("%Y-%m-%d"))
+            print("an error occured for "+hist.strftime("%Y-%m-%d"))
 
 
     for r in range(len(head)-1):
