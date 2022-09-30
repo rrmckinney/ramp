c031b024b767a1d6c173d53c568d95b4228d8b69
rrmckinney <rmkinneyey@eoas.ubc.ca>
2022-09-30 15:43:37 -0700

    update ramp data

--- docs/source/func.py
+++ docs/source/func.py
@@ -44,7 +44,7 @@ def run():
             hist = hist + timedelta(days=1)
     
     week_dat.index = pd.DatetimeIndex(week_dat.Date)
-    week_dat.resample('1Min')['Date'].mean()
+    week_dat.resample('10Min')['Date'].mean()
     for r in range(len(head)-1):
         plt.figure(figsize=(15,5))
         plt.scatter(week_dat['Date'],week_dat[str(head[r+1])], s=1)
